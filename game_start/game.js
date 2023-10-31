3d453fb0332ba8a24e24c02e44812dd13a2bc1f9
Mateusz Karpowicz <m.karpowicz@bluewom.com>
2023-10-31 15:50:38 +0100

    Add feedback

--- game_start/game.js
+++ game_start/game.js
@@ -1,26 +1,26 @@
 const btnPlay = document.querySelector('.startGame');
 const addCards = document.querySelector('.cards');
-let cards = [...document.querySelectorAll('.card')];
+let cards = document.querySelectorAll('.card');
 const moves = document.querySelector('.moves');
 const cardsArray = [
-	'pumpkin',
-	'pumpkin',
-	'babaJaga',
-	'babaJaga',
-	'house',
-	'house',
-	'scareCrow',
-	'scareCrow',
-	'ghost',
-	'ghost',
-	'happyHaloween',
-	'happyHaloween',
-	'shoes',
-	'shoes',
-	'web',
-	'web',
-	'bat',
-	'bat',
+  'pumpkin',
+  'pumpkin',
+  'babaJaga',
+  'babaJaga',
+  'house',
+  'house',
+  'scareCrow',
+  'scareCrow',
+  'ghost',
+  'ghost',
+  'happyHaloween',
+  'happyHaloween',
+  'shoes',
+  'shoes',
+  'web',
+  'web',
+  'bat',
+  'bat',
 ];
 
 const activeCards = [];
@@ -32,44 +32,42 @@ const result = 0;
 // logika gry
 
 const showCards = function () {
-	activeCard = this;
-	activeCard.classList.remove('hidden');
+  activeCard = this;
+  activeCard.classList.remove('hidden');
 
-	if (activeCards.length === 0) {
-		activeCards[0] = activeCard;
-		return;
-	} else {
-		cards.forEach((card) => card.removeEventListener('click', showCards));
-		activeCards[1] = activeCard;
+  if (activeCards.length === 0) {
+    activeCards[0] = activeCard;
 
-		setTimeout(function () {
-			if (activeCards[0].className === activeCards[1].className) {
-				activeCards.forEach((card) => {
-					return;
-				});
-			} else {
-				activeCards.forEach((card) => {
-					card.classList.add('hidden');
-				});
-			}
-			activeCard = '';
-			activeCards.length = 0;
-			cards.forEach((card) => card.addEventListener('click', showCards));
-		}, 1000);
-	}
+    return;
+  } else {
+    cards.forEach((card) => card.removeEventListener('click', showCards));
+    activeCards[1] = activeCard;
+
+    if (activeCards[0].className !== activeCards[1].className) {
+      setTimeout(function () {
+        activeCards.forEach((card) => {
+          card.classList.add('hidden');
+        });
+
+        activeCard = '';
+        activeCards.length = 0;
+
+        cards.forEach((card) => card.addEventListener('click', showCards));
+      }, 1000);
+    }
+  }
 };
 // licznik kliknięć myszką
 
 const mouseMove = () => {
-	let clicks = 0;
-	console.log(clicks);
+  let clicks = 0;
 
-	const click = () => {
-		clicks++;
-		moves.textContent = `Moves: ${clicks}`;
-	};
+  const click = () => {
+    clicks++;
+    moves.textContent = `Moves: ${clicks}`;
+  };
 
-	return click;
+  return click;
 };
 
 const clicksScore = mouseMove();
@@ -77,21 +75,21 @@ const clicksScore = mouseMove();
 // główna funckja
 
 const playGame = () => {
-	cards.forEach((card) => {
-		card.addEventListener('click', clicksScore);
+  cards.forEach((card) => {
+    card.addEventListener('click', clicksScore);
 
-		const position = Math.floor(Math.random() * cardsArray.length);
+    const position = Math.floor(Math.random() * cardsArray.length);
 
-		card.classList.add(cardsArray[position]);
-		cardsArray.splice(position, 1);
-	});
+    card.classList.add(cardsArray[position], 'card-general');
+    cardsArray.splice(position, 1);
+  });
 
-	setTimeout(function () {
-		cards.forEach((card) => {
-			card.classList.add('hidden');
-			card.addEventListener('click', showCards);
-		});
-	}, 800);
+  setTimeout(function () {
+    cards.forEach((card) => {
+      card.classList.add('hidden');
+      card.addEventListener('click', showCards);
+    });
+  }, 800);
 };
 
 btnPlay.addEventListener('click', playGame);
